eb30cbf09492bca1c778d0c6df655f0704a6d8cf
Zaid Malik <mzaidmalik25@gmail.com>
2026-07-17 21:14:31 +0530

    Add implementation for sum of diagonal elements in a 2D array

--- 2d-array/index.cpp
+++ 2d-array/index.cpp
@@ -81,3 +81,35 @@
 
 //     return 0;
 // }
+
+// sum of diagnol matrix;
+
+// #include<iostream>
+
+// using namespace std;
+
+// int main () {
+
+//     int arr[4][4] = {
+//                         {1, 2, 3, 4},
+//                         {5, 6, 7, 8},
+//                         {9, 10, 11, 12},
+//                         {13, 14, 15, 16}
+//                     };
+
+    
+//     int n = 4;
+//     int sum = 0;
+
+//     for(int i = 0; i < n; i++) {
+
+//         sum += arr[i][i];
+
+//         if(i != n-i-1) {
+//             sum+= arr[i][n-i-1];
+//         }
+//     }
+
+//     cout << "sum  = " << sum << endl;
+//     return 0;
+// }
