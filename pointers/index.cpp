93fc1893bd5604d2759148dfaed1422f6aebd953
Zaid Malik <mzaidmalik25@gmail.com>
2026-07-26 13:06:41 +0530

    Add lecture 6 examples for dynamic memory allocation with pointers

--- pointers/index.cpp
+++ pointers/index.cpp
@@ -324,3 +324,50 @@
 //     // cout << ptr << endl;
 //     // return 0;
 // }
+
+// lecture 6 pointers;
+
+// #include<iostream>
+
+// using namespace std;
+
+
+// int main () {
+
+//     int n, m;
+
+//     cout << "Enter the row : ";
+//     cin >> n;
+
+//     cout << "Enter the col : ";
+//     cin >> m;
+
+//     // create the address of array;
+
+//     int **ptr = new int *[n];
+
+//     for(int i = 0; i < n; i++) {
+//         ptr[i] = new int[m];
+//     }
+
+//     for(int i = 0; i < n; i++)
+//         for(int j = 0; j < m; j++)
+//             cin >> ptr[i][j];
+    
+//     for(int i = 0; i < n; i++) {
+//         for(int j = 0; j < m; j++) {
+//             cout << ptr[i][j] << " ";
+//             cout << endl;
+//         }
+//     }
+
+//     // release the memory;
+
+//     for(int i = 0; i < n; i++) {
+//         delete[] ptr[i];
+//     }
+
+//     delete[] ptr;
+
+//     return 0;
+// }
