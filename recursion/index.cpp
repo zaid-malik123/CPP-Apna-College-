0bbfa152aefffb38d2c2bb7fbe7d89ecd1c0e9a7
Zaid Malik <mzaidmalik25@gmail.com>
2026-08-18 21:36:23 +0530

    Comment out implementations and main functions for subsequence and sliding window examples; delete unused main file

--- recursion/index.cpp
+++ recursion/index.cpp
@@ -728,53 +728,78 @@
 
 // print the subsequence;
 
+// #include<iostream>
+
+// using namespace std;
+
+// void subsequence(int arr[], int n, int i, vector<vector<int>>&ans, vector<int> &temp) {
+
+//     if( i == n ) {
+//         ans.push_back(temp);
+//         return;
+//     }
+
+//     // not include;
+//     subsequence(arr, n, i+1, ans, temp);
+
+//     // include;
+//     temp.push_back(arr[i]);
+//     subsequence(arr, n, i+1, ans, temp);
+//     temp.pop_back();
+
+// }
+
+// int main () {
+
+//     int arr[] = { 1, 2, 3 };
+
+//     int n = 3;
+
+//     vector<int> temp;
+//     vector<vector<int>> ans;
+
+//     int i = 0;
+
+//     subsequence(arr, n, i, ans, temp);
+
+//     for ( int i = 0; i < ans.size(); i++ ) {
+
+//         for ( int j = 0; j < ans[i].size(); j++ ) {
+
+//             cout << ans[i][j] << " ";
+
+//         }
+
+//         cout << endl;
+//     }
+
+//     cout << "Your ans size is :- " << ans.size() << endl;
+
+//     return 0;
+
+// }
+
 #include<iostream>
 
 using namespace std;
 
-void subsequence(int arr[], int n, int i, vector<vector<int>>&ans, vector<int> &temp) {
+void subsetSum( int arr[], int n, int i, int sum) {
 
-    if( i == n ) {
-        ans.push_back(temp);
-        return;
-    }
+    if(i == n) return;
 
-    // not include;
-    subsequence(arr, n, i+1, ans, temp);
+    subsetSum(arr, n, i, sum);
 
-    // include;
-    temp.push_back(arr[i]);
-    subsequence(arr, n, i+1, ans, temp);
-    temp.pop_back();
-
+    subsetSum(arr, n, i, sum+=arr[i]);
 }
 
 int main () {
 
     int arr[] = { 1, 2, 3 };
-
     int n = 3;
-
-    vector<int> temp;
-    vector<vector<int>> ans;
-
+    int sum = 0;
     int i = 0;
 
-    subsequence(arr, n, i, ans, temp);
-
-    for ( int i = 0; i < ans.size(); i++ ) {
-
-        for ( int j = 0; j < ans[i].size(); j++ ) {
-
-            cout << ans[i][j] << " ";
-
-        }
-
-        cout << endl;
-    }
-
-    cout << "Your ans size is :- " << ans.size() << endl;
-
+    subsetSum(arr, n, i, sum);
     return 0;
 
 }
