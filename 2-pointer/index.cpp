466873cee8669b25db41097e9108e11b50beb217
Zaid Malik <mzaidmalik25@gmail.com>
2026-08-22 23:04:11 +0530

    solved two pointer question

---
+++ 2-pointer/index.cpp
@@ -0,0 +1,43 @@
+// #include<iostream>
+
+// using namespace std;
+
+// int main () {
+
+//     int arr[] = { 1, 1, 1, 2, 2, 3, 3, 3, 4 };
+
+//     int n = 9;
+
+//     int i = 0;
+
+//     int j = 1;
+
+//     while (j < n)
+//     {
+//         /* code */
+
+//         if( arr[j] == arr[j-1] ) {
+
+//             j++;
+//             continue;
+
+//         }
+
+//         else {
+
+//             swap(arr[i+1], arr[j]);
+//             i++;
+
+//         }
+//     }
+    
+
+//     for ( int k = 0; k < n; k++ ) {
+//         cout << arr[k] << " ";
+//     }
+
+//     cout << endl;
+
+//     return 0;
+
+// }
