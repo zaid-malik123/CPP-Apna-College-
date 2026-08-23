27f3df9a51214e669c494aa0484af4f84d627dd6
Zaid Malik <mzaidmalik25@gmail.com>
2026-08-23 14:57:46 +0530

    Comment out implementations and main functions for permutation and counting ways

--- recursion/index.cpp
+++ recursion/index.cpp
@@ -1066,3 +1066,104 @@
 
 //     return 0;
 // }
+
+// #include<iostream>
+
+// using namespace std;
+
+// void permutation(vector<int> &arr, int n, int idx, vector<vector<int>> &ans) {
+
+//     if ( idx == n ) {
+
+//          ans.push_back(arr);
+//          return;
+
+//     }
+
+//     vector<int> visit(21, 0);
+
+//     for ( int i = idx; i < n; i++ ) {
+
+//         if( visit[ arr[ i ] + 10 ] == 0 ) {
+
+//             swap(arr[i], arr[idx]);
+
+//             permutation(arr, n, idx + 1, ans);
+
+//             swap(arr[i], arr[idx]);
+
+//             visit[ arr[ i ] + 10 ] = 1;
+
+//         }
+
+//     }
+
+// }
+
+// int main() {
+
+//     vector<int> arr = { 1, 1, 2, 2 };
+
+//     int n = arr.size();
+
+//     int idx = 0;
+
+//     vector<vector<int>> ans;
+
+//     permutation(arr, n, idx, ans);
+
+//     for ( int i = 0; i < ans.size(); i++ ) {
+
+//         for( int j = 0; j < ans[0].size(); j++ ) {
+
+//             cout << ans[i][j] << " ";
+
+//         }
+
+//         cout << endl;
+
+//     }
+
+//     return 0;
+
+// }
+
+// #include<iostream>
+
+// using namespace std;
+
+// int countWays(int arr[], int n, int idx, int sum, int count) {
+
+//     if( sum < 0 || idx == n ) return 0;
+
+//     if( sum == 0 ) {
+
+//         count++;
+//         return 1;
+//     }
+
+//     for ( int i = 0; i < n; i++ ) {
+//         countWays(arr, n, idx+1, sum, count);
+//     }
+
+// }
+
+
+// int main () {
+
+//     int arr[] = {1, 5, 6};
+//     int n = 3;
+
+//     int sum = 7;
+
+//     int i  = 0;
+
+//     int count = 0;
+
+//     countWays(arr, n, i, sum, count);
+
+
+
+//     return 0;
+
+// }
