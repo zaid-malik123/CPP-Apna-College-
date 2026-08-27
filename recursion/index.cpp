80c360156aa7321d6b068e4eb79532e65fa14813
Zaid Malik <mzaidmalik25@gmail.com>
2026-08-27 17:10:02 +0530

    Add optimized approach for winner problem using recursion

--- recursion/index.cpp
+++ recursion/index.cpp
@@ -1233,3 +1233,31 @@
 //     return 0;
 
 // }
+
+// optimized approach;
+
+// #include<iostream>
+
+// using namespace std;
+
+// int winner(int n, int k) {
+
+//     if ( n == 1 ) {
+//         return 0;
+//     }
+
+//     return (winner(n-1, k) + k) % n;
+
+// }
+
+// int main () {
+
+//     int n = 5;
+
+//     int k = 3;
+
+//     cout << winner(n, k) << endl;
+
+//     return 0;
+
+// }
