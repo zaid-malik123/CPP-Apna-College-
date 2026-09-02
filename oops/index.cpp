644eb5c2c92c64086a7900a3a420682c5370091c
Zaid Malik <mzaidmalik25@gmail.com>
2026-09-02 20:38:03 +0530

    oops in c++

---
+++ oops/index.cpp
@@ -0,0 +1,61 @@
+// #include<iostream>
+
+// using namespace std;
+
+// class Student {
+
+    
+//         string name;
+//         string grade;
+//         int age;
+
+//     // getter & setter;
+
+//     public:
+
+//         void setName(string n) {
+//             name = n;
+//         }
+
+//         string getName() {
+//             return name;
+//         }
+    
+        
+// };
+
+// int main () {
+
+//     Student s1;
+
+//     s1.setName("zaid");
+
+//     cout << s1.getName() << endl;
+    
+//     return 0;
+
+// }
+
+// #include<iostream>
+
+// using namespace std;
+
+// class Student {
+
+//     public: 
+//         string name;
+// };
+
+// int main () {
+
+//     Student *s1 = new Student;
+
+//     (*s1).name = "zaid";
+
+//     // cout << (*s1).name;
+
+//     cout << s1->name;
+
+//     return 0;
+
+// }
