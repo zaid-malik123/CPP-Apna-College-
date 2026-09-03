ce4f54fa7ec2395642413a75286e18fb0d1a2771
Zaid Malik <mzaidmalik25@gmail.com>
2026-09-03 18:57:57 +0530

    Add Customer class with constructor and display method; implement static member for total customers

--- oops/index.cpp
+++ oops/index.cpp
@@ -169,3 +169,55 @@
 //     return 0;
 
 // }
+
+// static keyword ka use hum aise variable ya function ko define karne ke liye karte hain jo class ke sabhi objects ke liye common/shared ho.
+
+// #include<iostream>
+
+// using namespace std;
+
+// class Customer {
+
+//     string name;
+
+//     int accountNumber, balance;
+
+//     static int totalCustomer;
+
+//     public:
+//     Customer( string name, int accountNumber, int balance ) {
+
+//         this->name = name;
+//         this->balance = balance;
+//         this->accountNumber = accountNumber;
+
+//         totalCustomer++;
+//     }
+
+//     public:
+//     void display() {
+//         cout << "Your name is " << this->name
+//         << " and your account Number is " << this->accountNumber
+//         << " and your balance is " << this->balance
+//         << " and total customer in " << totalCustomer 
+//         << endl;
+//     }
+// };
+
+// int Customer::totalCustomer = 0;
+
+// int main () {
+
+//     Customer C1("Zaid", 1, 1000);
+
+//     Customer C2("Kaif", 2, 2000);
+
+//     Customer C3("Aafiya", 3, 4000);
+
+//     Customer C4("Zaibu", 4, 5000);
+
+//     C3.display();
+
+//     return 0;
+
+// }
