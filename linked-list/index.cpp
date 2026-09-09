e5c8970a4e9a2102963f9f7d8dc33b68ed09283d
Zaid Malik <mzaidmalik25@gmail.com>
2026-09-09 18:45:41 +0530

    Refactor linked list implementation: add insertion and traversal functionality

--- linked-list/index.cpp
+++ linked-list/index.cpp
@@ -68,3 +68,71 @@
     
 //     return 0;
 // }
+
+// Revision Linked List Start Inserting & Traversing;
+
+// #include<iostream>
+
+// using namespace std;
+
+// class Node {
+
+//     public:
+//         int data;
+
+//         Node *next;
+
+//         Node( int v ) { 
+
+//             data = v;
+//             next = NULL;
+//         } 
+// };
+
+// int main () {
+
+//     int arr[] = { 1, 2, 3, 4 };
+
+//     Node *Head;
+
+//     Head = NULL;
+
+//     for ( int i = 0; i < 4; i++ ) {
+
+//         if( Head == NULL ) {
+
+//             Head = new Node(arr[i]);
+
+//         }
+
+//         else {
+
+//            Node *temp;
+
+//            temp = new Node(arr[i]);
+
+//            temp->next = Head;
+
+//            Head = temp;
+
+//         }
+//     }
+
+//     Node *temp = Head;
+
+//     while (temp)
+
+//     {
+//         /* code */
+
+//         cout << temp->data << " ";
+
+//         temp = temp->next;
+
+//     }
+    
+//     cout << endl;
+
+//     return 0;
+
+// }
