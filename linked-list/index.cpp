c700449f9fd884eb13502c0883a8dfd6290059e1
Zaid Malik <mzaidmalik25@gmail.com>
2026-09-12 17:14:16 +0530

    Add linked list implementation with node creation and traversal functionality

--- linked-list/index.cpp
+++ linked-list/index.cpp
@@ -412,3 +412,70 @@
 //     return 0;
 
 // }
+
+// #include<iostream>
+
+// using namespace std;
+
+// class Node {
+
+//     public:
+
+//         int data;
+
+//         Node *next;
+
+//         Node( int val ) {
+
+//             data = val;
+//             next = NULL;
+
+//         }
+// };
+
+// int main () {
+
+//     int arr[] = { 1, 2, 3, 4, 5 };
+
+//     int n = 5;
+
+//     Node *Head = NULL;
+
+//     for ( int i = 0; i < n; i++ ) {
+
+//         if( Head == NULL ) {
+
+//             Head = new Node(arr[i]);
+
+//         }
+
+//         else {
+
+//             Node *temp;
+
+//             temp = new Node(arr[i]);
+
+//             temp->next = Head;
+
+//             Head = temp;
+            
+//         }
+//     }
+
+//     Node *temp = Head;
+
+//     while (temp)
+//     {
+//         /* code */
+
+//         cout << temp->data << " ";
+
+//         temp = temp->next;
+
+//     }
+    
+//     cout << endl;
+
+//     return 0;
+
+// }
